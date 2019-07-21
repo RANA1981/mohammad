27e21d85353b5b287fe2961ca1a20d02f41f0176
Rena hayrat <irfan.gt11@gmail.com>
2019-07-21 17:01:38 -0400

    rana

---
+++ src/techcircle/MyFirstJavaFile.java
@@ -0,0 +1,41 @@
+package techcircle;
+
+import java.util.*;
+
+public class MyFirstJavaFile {
+	public static void main(String[] args) {
+
+		System.out.println("Hello Rena");
+
+		String firstKid = "Michael";
+
+		String secondKid;
+		secondKid = "Mustafa";
+
+		String thirdKid = "Muniba";
+
+		System.out.println(
+				"I have three beautiful kids and they are, " + firstKid + "," + secondKid + "," + thirdKid + ".");
+
+		System.out.println("I have three beautiful kids and they are, Mohammad, Mustafa,Muniba.");
+
+		System.out.println(
+				"I have three beautiful kids and they are, " + firstKid + ", " + secondKid + "," + thirdKid + ".");
+		
+		boolean myFirstBoolean = true;
+		boolean mySecondBoolean = false;
+		
+		
+		double doublecity =5.1;
+		System.out.println(doublecity);
+		int someInt =(int)doublecity;
+		System.out.println(someInt);
+		double city = doublecity;
+		System.out.println(city);
+		
+		
+		
+	 
+	 
+	}
+} 
---
+++ src/techcircle_learning/JavaClass1.java
@@ -0,0 +1,7 @@
+package techcircle_learning;
+
+public class JavaClass1 {
+public static void main(String[] args) {
+	System.out.println("hello world");
+}
+}
